4764b8180037575b6860b8a5859f57e1083ceb5a
Andriy Kushynskyy <akushyn@gmail.com>
2018-01-28 21:24:27 +0200

    remove useless comments

--- plotDistribution.py
+++ plotDistribution.py
@@ -7,29 +7,6 @@ Created on Sun Jan 28 20:27:27 2018
 import distribution as distr
 import matplotlib.pyplot as plt
 
-'''
-plt.figure()
-plt.title(distr.header[0])
-plt.hist(distr.highLow, bins=20)
-
-plt.figure()
-plt.title(distr.header[1])
-plt.hist(distr.openHigh, bins='auto')
-
-plt.figure()
-plt.title(distr.header[2])
-plt.hist(distr.openLow, bins='auto')
-
-plt.figure()
-plt.title(distr.header[3])
-plt.hist(distr.highClose, bins='auto')
-
-plt.figure()
-plt.title(distr.header[4])
-plt.hist(distr.lowClose, bins='auto')
-
-'''
-
 # cycle on distributions and plot 
 for i in range(len(distr.distributions)):
     plt.figure()

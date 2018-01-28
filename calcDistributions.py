350c3f29f635916725d7156720790b329125dd0c
Andriy Kushynskyy <akushyn@gmail.com>
2018-01-28 14:47:50 +0200

    test

---
+++ calcDistributions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 20:31:28 2018
+
+@author: Andriy
+"""
+import loadCSV as csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+import plotly.plotly as py
+x = np.random.randn(500)
+
+print(csv.data)
+
+'''highLow
+
+plt.hist(csv.data)
+plt.title("Gaussian Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+'''

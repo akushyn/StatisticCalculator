ef6e4c4314412544990231942969df36c1035370
Andriy Kushynskyy <akushyn@gmail.com>
2018-01-30 11:51:52 +0200

    Create new form and buttons for the test.

--- testPython.py
+++ testPython.py
@@ -5,6 +5,12 @@ Created on Mon Jan 29 19:32:43 2018
 @author: Andriy
 """
 
-import time
-ticks = time.time()
-print(ticks)
+import tkinter as tk
+tk.root = tk.Tk()
+tk.Button(tk.root, text = '1').pack(side = 'left')
+tk.Button(tk.root, text = '2').pack(side = 'top')
+tk.Button(tk.root, text = '3').pack(side = 'right')
+tk.Button(tk.root, text = '4').pack(side = 'bottom')
+tk.Button(tk.root, text = '5').pack(fill = 'both')
+
+tk.root.mainloop()
